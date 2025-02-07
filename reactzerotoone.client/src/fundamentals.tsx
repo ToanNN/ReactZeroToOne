4a609de0158f1cd26ca75f2fb136c6326aaa90b1
Toan Nguyen <toan.nguyen@qr.com.au>
2025-02-07 16:54:01 +1000

    Add react thinking

--- reactzerotoone.client/src/fundamentals.tsx
+++ reactzerotoone.client/src/fundamentals.tsx
@@ -1,18 +1,12 @@
-
 // probs are passed down from parent to child
-//To collect data from multiple children, or to have two child components communicate with each other, 
+//To collect data from multiple children, or to have two child components communicate with each other,
 //declare the shared state in their parent component instead.
 //The parent component can pass that state back down to the children via props.
 //This keeps the child components in sync with each other and with their parent.
 
 //It’s strongly recommended that you assign proper keys whenever you build dynamic lists
-function MyButton({ count , onClick}) {
-
-    return (
-        <button onClick={onClick}>
-            Clicked {count} times
-        </button>
-    );
+function MyButton({ count, onClick }) {
+  return <button onClick={onClick}>Clicked {count} times</button>;
 }
 
 export { MyButton };
