3e5778bea9635e7b358bba22baf90cfe2de18e6a
Toan Nguyen <toan.nguyen@qr.com.au>
2025-02-07 16:04:56 +1000

    Implement the game and calculate the winner

--- reactzerotoone.client/src/tictactoe/dashboard.tsx
+++ reactzerotoone.client/src/tictactoe/dashboard.tsx
@@ -1,49 +1,113 @@
 import { useState } from "react";
 
-function Square({value, onSquareClick}) {
-   
-
+function Square({ value, onSquareClick }) {
   return (
     <button
       className="square"
-      onClick={onSquareClick}
-     >
+      onClick={onSquareClick}>
       {value}
     </button>
   );
 }
 
 export default function Board() {
-
   const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
 
-  function handleClick(i){
-    // create a copy of the squares
-    const nextSquares = squares.slice()
+  function handleClick(i) {
+    if (squares[i] || calculateWinner(squares)) {
+      return;
+    }
 
-    nextSquares[i] ='X';
+    // create a copy of the squares to avoid in place state change
+    const nextSquares = squares.slice();
 
+    // Alternate state
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
     // Calling set method lets React know the state of the component has changed
     // Trigger a re-render of the components that use the squares state (Board) as well as its child components (the Square components that make up the board).
-    setSquares(nextSquares)
+    setSquares(nextSquares);
+
+    setXIsNext(!xIsNext);
   }
-// You cannot invoke function directly handleClick(0). YOu have to define an event handler
+
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+
+  // You cannot invoke function directly handleClick(0). YOu have to define an event handler
   return (
     <>
-      <div className="board-row">        
-        <Square value ={squares[0]} onSquareClick={()=>handleClick(0)} />
-        <Square value ={squares[1]} onSquareClick={()=>handleClick(1)} />
-        <Square value ={squares[2]} onSquareClick={()=>handleClick(2)} />
+      <div className="status">{status}</div>
+      <div className="board-row">
+        <Square
+          value={squares[0]}
+          onSquareClick={() => handleClick(0)}
+        />
+        <Square
+          value={squares[1]}
+          onSquareClick={() => handleClick(1)}
+        />
+        <Square
+          value={squares[2]}
+          onSquareClick={() => handleClick(2)}
+        />
       </div>
       <div className="board-row">
-        <Square value ={squares[3]} onSquareClick={()=>handleClick(3)} />
-        <Square value ={squares[4]} onSquareClick={()=>handleClick(4)} />
-        <Square value ={squares[5]}  onSquareClick={()=>handleClick(5)}/>
+        <Square
+          value={squares[3]}
+          onSquareClick={() => handleClick(3)}
+        />
+        <Square
+          value={squares[4]}
+          onSquareClick={() => handleClick(4)}
+        />
+        <Square
+          value={squares[5]}
+          onSquareClick={() => handleClick(5)}
+        />
       </div>
       <div className="board-row">
-        <Square  value ={squares[6]} onSquareClick={()=>handleClick(6)}/>
-        <Square  value ={squares[7]} onSquareClick={()=>handleClick(7)}/>
-        <Square  value ={squares[8]} onSquareClick={()=>handleClick(8)}/>
+        <Square
+          value={squares[6]}
+          onSquareClick={() => handleClick(6)}
+        />
+        <Square
+          value={squares[7]}
+          onSquareClick={() => handleClick(7)}
+        />
+        <Square
+          value={squares[8]}
+          onSquareClick={() => handleClick(8)}
+        />
       </div>
     </>
   );
