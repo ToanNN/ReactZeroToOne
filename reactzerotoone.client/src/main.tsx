74f98fd597f873929ef9abd9dcb188df751fa477
Toan Nguyen <toan.nguyen@qr.com.au>
2025-02-07 15:25:43 +1000

    Add board game

--- reactzerotoone.client/src/main.tsx
+++ reactzerotoone.client/src/main.tsx
@@ -1,6 +1,11 @@
+// React
 import { StrictMode } from 'react'
+
+// React browser DOM
 import { createRoot } from 'react-dom/client'
+// styles
 import './index.css'
+// App component
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
---
+++ reactzerotoone.client/src/tictactoe/tictactoe.tsx
@@ -0,0 +1,11 @@
+function tictactoe() {
+    return (
+        
+  );
+}
+
+export default function Square() {
+    return <button className="square">X</button>;
+}
+
+export default tictactoe;
